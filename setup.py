1881aae4c7a2f27d48826b1366f4ea61bac0254a
Angel Velasquez <angvp@archlinux.org>
2013-12-12 18:35:02 -0300

    Prepare for release

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read_file(filename):
 setup(
     name='django-oml',
     version=__import__('oml').__version__,
-    author=u'Ángel Velásquez, Agustín Cangiani',
+    author=u'Angel Velasquez, Agustín Cangiani',
     author_email='angvp@archlinux.org, cangiani@gmail.com',
     packages=find_packages(),
     include_package_data=True,

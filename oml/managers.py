dc2f646d4545c3b268615797e059b7bb021dac75
Angel Velasquez <angvp@archlinux.org>
2014-10-15 09:21:37 -0300

    Closes #2

--- oml/managers.py
+++ oml/managers.py
@@ -7,14 +7,14 @@ class ModeratedModelQuerySet(models.query.QuerySet):
 
 
 class ModeratedModelManager(models.Manager):
-    def get_query_set(self):
+    def get_queryset(self):
         return ModeratedModelQuerySet(self.model, using=self._db)
 
     def accepted(self):
-        return self.get_query_set().main_queryset(status='a')
+        return self.get_queryset().main_queryset(status='a')
 
     def rejected(self):
-        return self.get_query_set().main_queryset(status='r')
+        return self.get_queryset().main_queryset(status='r')
 
     def pending(self):
-        return self.get_query_set().main_queryset(status='p')
+        return self.get_queryset().main_queryset(status='p')

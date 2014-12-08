1dfe566efe724bf5de00d71e547fb905fd946c67
Angel Velasquez <angvp@archlinux.org>
2014-12-08 13:19:41 -0300

    Add python3 support

--- oml/models.py
+++ oml/models.py
@@ -4,7 +4,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.core import serializers
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from managers import ModeratedModelManager
+from .managers import ModeratedModelManager
 
 
 try:
